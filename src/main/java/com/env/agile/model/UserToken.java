d454df9d4d87f65a3555dcbdde462415a15d247d
gurusharangupta <gupta.gurusharan@gmail.com>
2019-08-06 21:10:10 +0530

    Changes to code

--- src/main/java/com/env/agile/model/UserToken.java
+++ src/main/java/com/env/agile/model/UserToken.java
@@ -5,12 +5,9 @@ import java.util.UUID;
 public class UserToken {
 
 	private String email;
-
-	private UUID _token;
 	
 	private String message;
 	
-	private String expiresIn;
 
 	public String getEmail() {
 		return email;
@@ -20,13 +17,6 @@ public class UserToken {
 		this.email = email;
 	}
 
-	public UUID get_token() {
-		return _token;
-	}
-
-	public void set_token(UUID _token) {
-		this._token = _token;
-	}
 
 	public String getMessage() {
 		return message;
@@ -36,12 +26,5 @@ public class UserToken {
 		this.message = message;
 	}
 
-	public String getExpiresIn() {
-		return expiresIn;
-	}
-
-	public void setExpiresIn(String expiresIn) {
-		this.expiresIn = expiresIn;
-	}
 
 }
