13109995b3a17a52097ec323b8d1b4b808a653b9
gurusharangupta <gupta.gurusharan@gmail.com>
2019-08-06 20:35:12 +0530

    Working h2 console

--- src/main/java/com/env/agile/config/authorization/SecurityConfigurer.java
+++ src/main/java/com/env/agile/config/authorization/SecurityConfigurer.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
